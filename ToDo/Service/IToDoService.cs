ff4b876396660acd65df506fb81f11bc31d138b1
Gustav Nyberg <gustav.nyberg@outlook.com>
2015-12-13 18:34:04 +0100

    The application is now fully functional. In this commit I have added code for the client and also implemented the first function in the service GetToDoListByName.

---
+++ ToDo/Service/IToDoService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using DataModel;
+
+namespace Service
+{
+    [ServiceContract]
+    public interface IToDoService
+    {
+        [OperationContract]
+        [WebGet]
+        List<ToDo> GetToDoListByName(string name);
+    }
+}
---
+++ ToDo/Service/ToDoService.svc.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using DataModel;
+using BusinessLogic;
+
+namespace Service
+{
+    public class ToDoService : IToDoService
+    {
+        public List<ToDo> GetToDoListByName(string name)
+        {
+            var toDoList = BusinessLogicLayer.GetToDoListByName(name);
+            return toDoList;
+        }
+    }
+}
