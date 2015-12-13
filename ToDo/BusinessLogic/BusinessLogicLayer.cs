386dc2eb032608ef6941f42a9f5f8dc27319640c
Gustav Nyberg <gustav.nyberg@outlook.com>
2015-12-13 20:08:53 +0100

    Missed these files.

---
+++ ToDo/BusinessLogic/BusinessLogicLayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using DataModel;
+using DataAccess;
+
+namespace BusinessLogic
+{
+    public class BusinessLogicLayer
+    {
+        public static List<ToDo> GetToDoListByName(string name)
+        {
+            var dbSession = new DataAccessLayer();
+            var toDoList = dbSession.GetToDoListByName(name);
+            return toDoList;
+        }
+    }
+}
---
+++ ToDo/Client/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Description;
+using Service;
+
+namespace Client
+{
+    internal class Client
+    {
+        private static void Main(string[] args)
+        {
+            WebServiceHost host = new WebServiceHost(typeof(ToDoService), new Uri("http://localhost:8000/"));
+            try
+            {
+                ServiceEndpoint ep = host.AddServiceEndpoint(typeof(IToDoService), new WebHttpBinding(), "");
+                host.Open();
+
+                using (ChannelFactory<IToDoService> cf = new ChannelFactory<IToDoService>(new WebHttpBinding(),"http://localhost:8000"))
+                {
+                    cf.Endpoint.Behaviors.Add(new WebHttpBehavior());
+
+                    IToDoService channel = cf.CreateChannel();
+
+                    Console.WriteLine("Calling GetToDoListByName via HTTP GET: ");
+                    var toDoList = channel.GetToDoListByName("Hamid");
+                    foreach (var toDo in toDoList)
+                    {
+                        Console.WriteLine("   Output: {0}", toDo.Description);
+                    }
+
+                    Console.WriteLine("");
+                    Console.WriteLine("This can also be accomplished by navigating to");
+                    Console.WriteLine("http://localhost:8000/GetToDoListByName?name=Hamid");
+                    Console.WriteLine("in a web browser while this sample is running.");
+
+                    Console.WriteLine("");
+                }
+
+                Console.WriteLine("Press <ENTER> to terminate");
+                Console.ReadLine();
+
+                host.Close();
+            }
+
+            catch (CommunicationException cex)
+            {
+                Console.WriteLine("An exception occurred: {0}", cex.Message);
+                host.Abort();
+            }
+        }
+    }
+}
